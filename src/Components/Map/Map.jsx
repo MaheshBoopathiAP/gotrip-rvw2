d5f78eb88e790123025e3c2433d2f632b8a715a8
MaheshBoopathiAP <727821tuit055@skct.edu.in>
2022-12-07 16:01:36 +0530

    adding all

---
+++ src/Components/Map/Map.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import GoogleMapReact from 'google-map-react'
+import { Paper,Typography,useMediaQuery } from '@material-ui/core'
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined'
+import { Rating } from '@material-ui/lab'
+import useStyles from'./Styles'
+const Map =() => {
+  const classes=useStyles();
+  const isMobile = useMediaQuery('(min-width:600)')
+  const coordinates={ lat:0, lng:0 }
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+      bootstrapURLKeys={{key:'AIzaSyCwlq6oQ_KptYBDGn7w2y9rMeJ5qQJJ1RM'}}
+      defaultCenter={coordinates}
+      center={coordinates}
+      defaultZoom={14}
+      margin={[50,50,50,50]}
+      options={''}
+      onChange={''}
+      onChildClick={''}
+      >
+      </GoogleMapReact>
+
+    </div>
+  )
+}
+
+export default Map
